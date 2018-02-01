da9885c5d3d0fe2f1cdd784f566f3620d4f68836
Samuel <Samuelfultz7@gmail.com>
2018-02-01 16:45:01 -0500

    Pace Enum 2/1/2018

--- src/byui/cit260/mfbMormonTrail/model/Pace.java
+++ src/byui/cit260/mfbMormonTrail/model/Pace.java
@@ -10,37 +10,10 @@ package byui.cit260.mfbMormonTrail.model;
  * @author samue
  */
 public enum Pace {
-    
-    /**
-     *
-     */
-    Pace1("Fast"),
-    Pace2("Slow"),
-    Pace3("Average");
-    
-    private String Fast;
-    private String Slow;
-    private String Average;
-
-    public String getFast() {
-        return Fast;
-    }
-
-    public String getSlow() {
-        return Slow;
-    }
-
-    public String getAverage() {
-        return Average;
-    }
-    
-
-    
-    
-    
-    
-    
-            
+     fast,
+     slow,
+     average;
+              
     
 }
 
