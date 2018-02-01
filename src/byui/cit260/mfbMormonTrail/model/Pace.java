813237aadc197c0122fba5fd922e6aa5a968b0bd
Samuel <Samuelfultz7@gmail.com>
2018-02-01 16:34:15 -0500

    Pace Enum 2/1/2018

---
+++ src/byui/cit260/mfbMormonTrail/model/Pace.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.mfbMormonTrail.model;
+
+/**
+ *
+ * @author samue
+ */
+public enum Pace {
+    
+    /**
+     *
+     */
+    Pace1("Fast"),
+    Pace2("Slow"),
+    Pace3("Average");
+    
+    private String Fast;
+    private String Slow;
+    private String Average;
+
+    public String getFast() {
+        return Fast;
+    }
+
+    public String getSlow() {
+        return Slow;
+    }
+
+    public String getAverage() {
+        return Average;
+    }
+    
+
+    
+    
+    
+    
+    
+            
+    
+}
+
+
+    
