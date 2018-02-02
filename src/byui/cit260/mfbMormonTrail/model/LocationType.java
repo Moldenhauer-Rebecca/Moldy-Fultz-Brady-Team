866a13abd590660bcf3d42506a8ba509bc075205
crmol <crmol@DESKTOP-IQD1F8K>
2018-02-01 17:32:51 -0700

    1/30/2018 enumeration Location Type update

--- src/byui/cit260/mfbMormonTrail/model/LocationType.java
+++ src/byui/cit260/mfbMormonTrail/model/LocationType.java
@@ -10,8 +10,47 @@ package byui.cit260.mfbMormonTrail.model;
  * @author crmol
  */
 public enum LocationType {
-    Town,
-    Fort,
-    River,
-    Landmark;
+    Town("Town", "Town is where you can visit the Hotel, General Store or return to the trail"),
+    Fort("Fort", "Fort is where you can buy and sell resources, or return to the trail"),
+    River("River", "Your team has reached a river crossing"),
+    HuntGather("HuntGather", "You are given the opportunity to either hunt or gather here");
+    
+    private final String name;
+    private final String description;
+    
+    LocationType(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public static LocationType getTown() {
+        return Town;
+    }
+
+    public static LocationType getFort() {
+        return Fort;
+    }
+
+    public static LocationType getRiver() {
+        return River;
+    }
+
+    public static LocationType getHuntGather() {
+        return HuntGather;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationType{" + "name=" + name + ", description=" + description + '}';
+    }
+     
+    
 }
