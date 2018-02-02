cc122a0e48d5b28e1edc3a3e6770c85f723d04f6
Mike <Mike@BradyPC.T-mobile.com>
2018-02-01 18:38:10 -0700

    Add LocationType and Pace to Project

---
+++ src/byui/cit260/mfbMormonTrail/model/LocationType.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.mfbMormonTrail.model;
+
+/**
+ *
+ * @author crmol
+ */
+public enum LocationType {
+    Town("Town", "Town is where you can visit the Hotel, General Store or return to the trail"),
+    Fort("Fort", "Fort is where you can buy and sell resources, or return to the trail"),
+    River("River", "Your team has reached a river crossing"),
+    HuntGather("HuntGather", "You are given the opportunity to either hunt or gather here");
+    
+    private final String name;
+    private final String description;
+    
+    LocationType(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public static LocationType getTown() {
+        return Town;
+    }
+
+    public static LocationType getFort() {
+        return Fort;
+    }
+
+    public static LocationType getRiver() {
+        return River;
+    }
+
+    public static LocationType getHuntGather() {
+        return HuntGather;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "LocationType{" + "name=" + name + ", description=" + description + '}';
+    }
+     
+    
+}
---
+++ src/byui/cit260/mfbMormonTrail/model/Pace.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.mfbMormonTrail.model;
+
+ 
+public enum Pace {
+    
+    Pace1("LevelOne", "slow pace"),
+    Pace2("LevelTwo", "average pace"),
+    Pace3("LevelThree", "fast pace");
+   
+    private String level;
+    private String description; 
+
+Pace(String level, String description){
+    this.level = level;
+    this.description = description;
+
+}
+
+    public static Pace getPace1() {
+        return Pace1;
+    }
+
+    public static Pace getPace2() {
+        return Pace2;
+    }
+
+    public static Pace getPace3() {
+        return Pace3;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Pace{" + "level=" + level + ", description=" + description + '}';
+    }
+
+
+    
+}
