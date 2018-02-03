df2a9919921188d74f1a9b928d2a147a9334bd73
Samuel <Samuelfultz7@gmail.com>
2018-02-03 17:15:31 -0500

    mfbMormonTrail class 2/1/2018

--- src/mormontrail/MormonTrail.java
+++ src/mormontrail/MormonTrail.java
@@ -5,18 +5,62 @@
  */
 package mormontrail;
 
+
+import byui.cit260.mfbMormonTrail.model.HuntingGathering;
+import byui.cit260.mfbMormonTrail.model.RiverScene;
 import byui.cit260.mfbMormonTrail.model.EventsScene;
+
+
+/**
+ *
+ * @author samuel
+ */
 public class MormonTrail {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        EventsScene eventsscene;
-      eventsscene = new EventsScene();
-      eventsscene.noToAnswer();
+
+       //HuntingGathering Class Stats 
+       
+       HuntingGathering huntingGathering = new HuntingGathering();
+       
+       huntingGathering.setTeamSkillLevel(5);
+       huntingGathering.setResourceAvailablity("tools");
+       
+       System.out.println(huntingGathering.toString());
+       
+       //**********
+       //RiverScene Class
+       
+       RiverScene riverScene = new RiverScene();
+       
+     riverScene.setOxyenStrength(50);
+     riverScene.setRiverDepth(4);
+     riverScene.setSuccessRate("Is my Wagon to heavy?");
+     riverScene.setWagonWeight(40);
+
+       System.out.println(riverScene.toString());
+       
+      
+       //**********
+       //EventsScene Class
+       
+       EventsScene eventsScene = new EventsScene();
+       
+       eventsScene.setNoToAnswer("yes or no");
+       eventsScene.setSuccessRate("Do I really want to shoot that Buffalo?");
+      
+       
+       System.out.println(eventsScene.toString());
+              
+       
+      
         
-        System.out.println(eventsscene.toString());
+        
     }
     
+         
+ 
 }
