af6b06241b5002ee549888865cc7e3208c994dc7
Samuel <Samuelfultz7@gmail.com>
2018-02-17 00:18:47 -0500

    2/17/18 CalcHuntingSuccessRate

---
+++ src/byui/cit260/mfbMormonTrail/control/CalcHuntingSuccessRate.java
@@ -0,0 +1,32 @@
+
+
+package byui.cit260.mfbMormonTrail.control;
+
+/**
+ *
+ * @author samue
+ */
+public class CalcHuntingSuccessRate {
+
+    
+    
+    public static double calcHuntingSuccessRate(int teamSkillLevel, int resourceAvailablity) {
+        
+        if (teamSkillLevel < 1 || teamSkillLevel > 3) {
+            return -1;
+        }
+    
+        if (resourceAvailablity < 1 || resourceAvailablity > 3) { 
+            return -2;
+        }
+        
+         int huntingSuccessRate = (teamSkillLevel + resourceAvailablity)*10;
+    
+        
+        return huntingSuccessRate;
+        
+    }
+ }
+    
+
+    
