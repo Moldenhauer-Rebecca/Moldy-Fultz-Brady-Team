1a53db9c60e181e03aa964469968955f09a61b6c
Samuel <Samuelfultz7@gmail.com>
2018-03-09 21:47:21 -0500

    GeneralStoreScene Update  3/9/18

--- src/byui/cit260/mfbMormonTrail/model/GeneralStoreScene.java
+++ src/byui/cit260/mfbMormonTrail/model/GeneralStoreScene.java
@@ -5,33 +5,83 @@
  */
 package byui.cit260.mfbMormonTrail.model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  *
  * @author crmol
  */
-public class GeneralStoreScene {
+public class GeneralStoreScene implements Serializable {
     
-    private ArrayList<InventoryItem> inventoryItem = new ArrayList<>();
-
-    public ArrayList<InventoryItem> getInventoryItem() {
-        return inventoryItem;
+    private ArrayList<InventoryItem> inventoryItems;
+    public ArrayList <InventoryItem> getInventoryItem(){
+       return inventoryItems;
     }
-
+    
     public void setInventoryItem(ArrayList<InventoryItem> inventoryItem) {
-        this.inventoryItem = inventoryItem;
+        this.inventoryItems = inventoryItems;
     }
     
+    //class instant variables
+    private String addInventoryItem;
     
-    public GeneralStoreScene() {
-        InventoryItem oxen = new InventoryItem();
-        oxen.setInventoryType("Oxen");
-        inventoryItem.add(oxen);
+    public GeneralStoreScene() { 
+        ArrayList<InventoryItem> inventoryItems = new ArrayList <InventoryItem> ();
+        InventoryItem inventoryItem = new InventoryItem ();
+        inventoryItem.setInventoryType("Oxen");
+        inventoryItems.add(inventoryItem);
         
-        InventoryItem wagon = new InventoryItem();
-        wagon.setInventoryType("wagon");
-        inventoryItem.add(wagon);
+        inventoryItem = new InventoryItem ();
+        inventoryItem.setInventoryType("Wagon");
+        inventoryItems.add(inventoryItem);
+        
     }
     
-}
+     public String addInventoryItem() {
+        return addInventoryItem;
+    }
+
+    public void setAddInventoryItem (String addInventoryItem) {
+        this.addInventoryItem = addInventoryItem;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.addInventoryItem);
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "GeneralStoreScene{" + "addInventoryItem=" + addInventoryItem + '}';
+    }
+    
+    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final GeneralStoreScene other = (GeneralStoreScene) obj;
+        return Objects.equals(this.addInventoryItem, other.addInventoryItem);
+    }
+    
+
+
+  
+   
+
+   
+    }
+    
+
